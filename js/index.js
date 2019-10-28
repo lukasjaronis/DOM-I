acbf3a04731e25909c5b2754756bedd7868e11a3
lukasjaronis <jaronisluke13@gmail.com>
2019-10-28 17:40:28 -0400

    added comments for myself

--- js/index.js
+++ js/index.js
@@ -41,54 +41,58 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+
+
+
+
+
 // navbar
 
-let aTagText = document.querySelectorAll('nav a');
-console.log(aTagText);
+let aTagText = document.querySelectorAll('nav a'); // selects all the anchor tags in nav
 
-aTagText.forEach((element, index) => {
-  return element.innerHTML = siteContent.nav[`nav-item-${index + 1}`];
+aTagText.forEach((element, index) => { // for each array element aka the the anchor links ....)
+  return element.innerHTML = siteContent.nav[`nav-item-${index + 1}`]; // add the nav-item-(#) --> index (The index of the current element being processed in the array.) + 1
 })
 
 // navbar text to green
 
-aTagText.forEach((element, index) => {
+aTagText.forEach((element) => { // for each array element ^^ add the style of green
   return element.style.color = 'green';
 })
 
 
-let domINATOR = document.createElement("a");
-let domINATORText = document.createTextNode("domINATOR");
-domINATOR.prepend(domINATORText);
-domINATOR.style.color = "red";
-domINATOR.href = "www.google.com";
-domINATOR.style.fontWeight = "900";
+let domINATOR = document.createElement("a"); // creating an anchor element
+let domINATORText = document.createTextNode("domINATOR"); // creating a text for the anchor element
+domINATOR.prepend(domINATORText); // insert this element at the beginning (can't really go anywhere since its text inside anchor tag) 
+domINATOR.style.color = "red"; // style
+domINATOR.href = "www.google.com"; // attribute
+domINATOR.style.fontWeight = "900"; // style
 
-document.querySelector("nav").appendChild(domINATOR);
+document.querySelector("nav").appendChild(domINATOR); // we can use prepend to push it to the start of the nav, or appendChild (a DOM method) to push it to the back.
 
 // cta 
 
-let ctaSection = document.querySelector('.cta');
+let ctaSection = document.querySelector('.cta'); // selecting 
 ctaSection.style.margin = "5rem";
 
 
-let ctaH1 = document.querySelector('.cta-text h1');
-siteContent.cta.h1 = siteContent.cta.h1.split(" ").join("<br>");
+let ctaH1 = document.querySelector('.cta-text h1'); // selecting 
+siteContent.cta.h1 = siteContent.cta.h1.split(" ").join("<br>"); // splitting a string into an array of substrings, and then using linke break
 ctaH1.innerHTML = siteContent.cta.h1;
 
-let ctaBtn = document.querySelector(".cta-text button");
+let ctaBtn = document.querySelector(".cta-text button"); // selecting 
 ctaBtn.innerHTML = siteContent.cta.button;
 
-let ctaImg = document.getElementById('cta-img');
+let ctaImg = document.getElementById('cta-img'); // selecting 
 ctaImg.src = siteContent.cta["img-src"];
 
 // main content-top
 
-let topHead = document.querySelectorAll(".top-content h4");
+let topHead = document.querySelectorAll(".top-content h4"); // selecting 
 topHead[0].innerHTML = siteContent["main-content"]["features-h4"];
 topHead[1].innerHTML = siteContent["main-content"]["about-h4"];
 
-let topHeadText = document.querySelectorAll(".top-content p");
+let topHeadText = document.querySelectorAll(".top-content p"); // selecting 
 topHeadText[0].innerHTML = siteContent["main-content"]["features-content"];
 topHeadText[1].innerHTML = siteContent["main-content"]["about-content"];
 
@@ -96,22 +100,22 @@ document.getElementById("middle-img").src = siteContent["main-content"]["middle-
 
 // main-content-bottom
 
-let botHead = document.querySelectorAll(".bottom-content h4");
+let botHead = document.querySelectorAll(".bottom-content h4"); // selecting 
 botHead[0].innerHTML = siteContent["main-content"]["services-h4"];
 botHead[1].innerHTML = siteContent["main-content"]["product-h4"];
 botHead[2].innerHTML = siteContent["main-content"]["vision-h4"];
 
-let botHeadText = document.querySelectorAll(".bottom-content p");
+let botHeadText = document.querySelectorAll(".bottom-content p"); // selecting 
 botHeadText[0].innerHTML = siteContent["main-content"]["services-content"];
 botHeadText[1].innerHTML = siteContent["main-content"]["about-content"];
 botHeadText[2].innerHTML = siteContent["main-content"]["vision-content"];
 
 // contact + copyright
 
-let contactHead = document.querySelector(".contact h4");
+let contactHead = document.querySelector(".contact h4"); // selecting 
 contactHead.innerHTML = siteContent.contact["contact-h4"];
 
-let contactInfo = document.querySelectorAll(".contact p");
+let contactInfo = document.querySelectorAll(".contact p"); // selecting 
 contactInfo[0].innerHTML = siteContent.contact.address;
 contactInfo[1].innerHTML = siteContent.contact.phone;
 contactInfo[2].innerHTML = siteContent.contact.email;
